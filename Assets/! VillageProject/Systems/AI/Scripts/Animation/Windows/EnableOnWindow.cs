2419fa3a0669c1d8848d28be418053a33dde69ec
bkorshunov <mrdrakula164@gmail.com>
2025-04-20 02:25:52 +0500

    Окна анимации

---
+++ Assets/! VillageProject/Systems/AI/Scripts/Animation/Windows/EnableOnWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class EnableOnWindow : MonoBehaviour
+{
+    [SerializeField] private AnimationWindowListener _listener;
+    [SerializeField] private GameObject _object;
+
+    private void OnEnable()
+    {
+        _listener.OnEntered += HandleEntered;
+        _listener.OnExited += HandleExit;
+    }
+
+    private void OnDisable()
+    {
+        _listener.OnEntered -= HandleEntered;
+        _listener.OnExited -= HandleExit;
+    }
+
+    private void HandleEntered()
+    {
+        _object.SetActive(true);
+    }
+
+    private void HandleExit()
+    {
+        _object.SetActive(false);
+    }
+}
