f9f4535f80a20c4cce3fa9d9dcfce41bd024c292
bkorshunov <mrdrakula164@gmail.com>
2025-05-08 02:42:21 +0500

    Исправлен баг с сохранением валюты после перезапуска

--- Assets/! VillageProject/Systems/Economics/Scripts/ResourceVariable.cs
+++ Assets/! VillageProject/Systems/Economics/Scripts/ResourceVariable.cs
@@ -5,7 +5,8 @@ public class ResourceVariable : ScriptableObject
 {
     public Action AmountChanged;
     [field: SerializeField] public string Name { get; private set; }
-    [field: SerializeField] public uint Amount { get; private set; }
+    [field: SerializeField] public uint DefaultAmount { get; private set; }
+    [field: NonSerialized] public uint Amount { get; private set; }
     public void Increment(uint amount)
     {
         Amount += amount;
@@ -16,5 +17,10 @@ public class ResourceVariable : ScriptableObject
     {
         Amount -= amount;
         AmountChanged?.Invoke();
+    }
+
+    private void Awake()
+    {
+        Amount = DefaultAmount;
     }
 }
