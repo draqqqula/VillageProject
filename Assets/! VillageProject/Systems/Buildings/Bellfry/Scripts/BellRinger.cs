7de879648c0e28792c931a41b6005066b94471e1
bkorshunov <mrdrakula164@gmail.com>
2025-05-23 17:41:26 +0500

    Переработана анимация колокола

--- Assets/! VillageProject/Systems/Buildings/Bellfry/Scripts/BellRinger.cs
+++ Assets/! VillageProject/Systems/Buildings/Bellfry/Scripts/BellRinger.cs
@@ -6,16 +6,23 @@ using System;
 public class BellRinger : MonoBehaviour
 {
     [SerializeField] private GateAlarm _alarm;
-    [SerializeField] private Bell _bell;
+    [SerializeField] private BellSound _bell;
     [SerializeField] private float _smallAlarmInterval;
     [SerializeField] private float _bigAlarmInterval;
+    [SerializeField] private float _smallAlarmAmplitude;
+    [SerializeField] private float _bigAlarmAmplitude;
+    [SerializeField] private Vector3 _direction;
     private IDisposable _subscription;
     private Coroutine _ringing;
+
+    private float Interval => _alarm.Alarm.CurrentValue == GateAlarm.AlarmStatus.SmallAlarm ? _smallAlarmInterval : _bigAlarmInterval;
+    private float Amplitude => _alarm.Alarm.CurrentValue == GateAlarm.AlarmStatus.SmallAlarm ? _smallAlarmAmplitude : _bigAlarmAmplitude;
+    private int Force => _alarm.Alarm.CurrentValue == GateAlarm.AlarmStatus.SmallAlarm ? 1 : 2;
 
     private void Reset()
     {
         _alarm = GetComponent<GateAlarm>();
-        _bell = GetComponent<Bell>();
+        _bell = GetComponent<BellSound>();
     }
 
     private void OnEnable()
@@ -37,13 +44,20 @@ public class BellRinger : MonoBehaviour
         {
             if (_ringing == null)
             {
-                StartCoroutine(RingWithInterval());
+                _ringing = StartCoroutine(BellMove());
             }
         }
     }
 
-    private IEnumerator RingWithInterval()
+    private IEnumerator RingInTime(int force, float duration)
     {
+        yield return new WaitForSeconds(duration);
+        _bell.Ring(force);
+    }
+
+    private IEnumerator BellMove()
+    {
+        float t = 0;
         while (true)
         {
             if (_alarm.Alarm.CurrentValue == GateAlarm.AlarmStatus.Quiet)
@@ -51,10 +65,19 @@ public class BellRinger : MonoBehaviour
                 _ringing = null;
                 yield break;
             }
-            var isSmall = _alarm.Alarm.CurrentValue == GateAlarm.AlarmStatus.SmallAlarm;
-
-            _bell.Ring(isSmall ? 1 : 2);
-            yield return new WaitForSeconds(isSmall ? _smallAlarmInterval : _bigAlarmInterval);
+            t = 0;
+            var force = Force;
+            var intervalDuration = Interval;
+            var amplitude = Amplitude;
+            StartCoroutine(RingInTime(force, intervalDuration * 0.25f));
+            StartCoroutine(RingInTime(force, intervalDuration * 0.75f));
+            while (t < intervalDuration)
+            {
+                var a = Mathf.Sin(2 * (t / intervalDuration) * Mathf.PI) * amplitude;
+                transform.eulerAngles = _direction * a;
+                yield return new WaitForEndOfFrame();
+                t += Time.deltaTime;
+            }
         }
     }
 }
---
+++ Assets/! VillageProject/Systems/Buildings/Bellfry/Scripts/BellSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BellSound : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioWeak;
+    [SerializeField] private AudioSource _audioStrong;
+
+    public void Ring(int force)
+    {
+        if (force <= 0)
+        {
+            _audioWeak.Play();
+        }
+        else
+        {
+            _audioStrong.Play();
+        }
+    }
+}
