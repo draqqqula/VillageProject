334f7af0f22bc2c3f5e00d5a467b59e068b85f95
WaRDy <ivan.alchin5@mail.ru>
2025-10-26 15:51:21 +0500

    https://ims.cr5.space/app/p/7p2YwVX2/village-keeper/tasks/33
    Добавление отступа для границы стамины, добавление новых спрайтов UI

--- Assets/! VillageProject/UI/Gameplay/HUD/Stamina/StaminaDisplayV2.cs
+++ Assets/! VillageProject/UI/Gameplay/HUD/Stamina/StaminaDisplayV2.cs
@@ -9,54 +9,68 @@ public class StaminaDisplayV2 : SignalListener<StaminaSignalInvoker.StaminaHover
     private const float FILLING_TIME = 0.2f;
     private const string VAR_NAME = "_FillAmount";
     
-    [SerializeField] private Image _display;
-    [SerializeField] private Image _spendableBg;
+    [SerializeField] private Image _firstPlanDisplay;
+    [SerializeField] private Image _secondPlanDisplay;
+    [SerializeField] private Image _thirdPlanDisplay;
     
-    private Material _displayFillMaterial;
-    private Material _bgFillMaterial;
+    private Material _firstDisplayMaterial;
+    private Material _secondDisplayMaterial;
+    private Material _thirdDisplayMaterial;
     
     private Coroutine _coroutine;
 
     private void Awake()
     {
-        _displayFillMaterial = new Material(_display.material);
-        _display.material = _displayFillMaterial;
-        
-        _bgFillMaterial = new Material(_spendableBg.material);
-        _spendableBg.material = _bgFillMaterial;
+        _firstDisplayMaterial = CreateMaterialInstance(_firstPlanDisplay);
+        _secondDisplayMaterial = CreateMaterialInstance(_secondPlanDisplay);
+        _thirdDisplayMaterial = CreateMaterialInstance(_thirdPlanDisplay);
+    }
+
+    private Material CreateMaterialInstance(Image display)
+    {
+        var material = new Material(display.material);
+        display.material = material;
+        return material;
     }
 
     protected override void OnSignal(StaminaSignalInvoker.StaminaHoverSignal value)
     {
         if (value.Amount == 0) return;
-        var fillAmount = _displayFillMaterial.GetFloat(VAR_NAME);
-        _displayFillMaterial.SetFloat(VAR_NAME, fillAmount - value.Amount);
-        _displayFillMaterial.SetInt("_WithEdge", 0);
+        var fillAmount = _secondDisplayMaterial.GetFloat(VAR_NAME);
+        
+        ChangeFrontPlansDisplays(fillAmount - value.Amount);
+        _firstDisplayMaterial.SetInt("_WithEdge", 0);
     }
     
     protected override void OnSignal(StaminaSignalInvoker.StaminaChangedSignal value)
     {
-        if (_bgFillMaterial.GetFloat(VAR_NAME) > value.Value)
+        if (_thirdDisplayMaterial.GetFloat(VAR_NAME) > value.Value)
         {
             if (_coroutine != null) StopCoroutine(_coroutine);
-            _coroutine = StartCoroutine(ChangeSpendableBgRoutine(value.Value));
+            _coroutine = StartCoroutine(ChangeThirdDisplayRoutine(value.Value));
         }
-        else _bgFillMaterial.SetFloat(VAR_NAME, value.Value);
+        else _thirdDisplayMaterial.SetFloat(VAR_NAME, value.Value);
         
-        _displayFillMaterial.SetFloat(VAR_NAME, value.Value);
+        ChangeFrontPlansDisplays(value.Value);
     }
 
-    private IEnumerator ChangeSpendableBgRoutine(float toValue)
+    private void ChangeFrontPlansDisplays(float value)
+    {
+        _firstDisplayMaterial.SetFloat(VAR_NAME, value);
+        _secondDisplayMaterial.SetFloat(VAR_NAME, value);
+    }
+
+    private IEnumerator ChangeThirdDisplayRoutine(float toValue)
     {
         var progress = 0f;
-        var fromValue = _bgFillMaterial.GetFloat(VAR_NAME);
+        var fromValue = _thirdDisplayMaterial.GetFloat(VAR_NAME);
         
         while (progress < FILLING_TIME)
         {
             progress += Time.deltaTime;
-            _bgFillMaterial.SetFloat(VAR_NAME, Mathf.Lerp(fromValue, toValue, progress / FILLING_TIME)); 
+            _thirdDisplayMaterial.SetFloat(VAR_NAME, Mathf.Lerp(fromValue, toValue, progress / FILLING_TIME)); 
             yield return null;
         }
-        _displayFillMaterial.SetInt("_WithEdge", 1);
+        _firstDisplayMaterial.SetInt("_WithEdge", 1);
     }
 }
