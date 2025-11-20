746714db1894a58b8ad1d4e30c8137844f8007cf
bkorshunov <mrdrakula164@gmail.com>
2025-11-21 03:24:07 +0500

    https://ims.cr5.space/app/p/7p2YwVX2/village-keeper/tasks/66
    Слабые точки теперь уничтожаются при попадании

---
+++ Assets/! VillageProject/Features/Combat/Damage/Scripts/AttributeSystem/Attributes/DestroyWeakSpot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Zenject;
+
+[GenerateDamageAttribute(inject: true)]
+public class DestroyWeakSpot : IDamageExecutable
+{
+    [Inject] private WeakSpotController _weakSpotController;
+
+    public bool TryExecute(DamageInteractionContext context)
+    {
+        _weakSpotController.Close();
+        return true;
+    }
+}
