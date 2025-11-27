825be7109fdc3dc477dd32df40e1131a676d9b2a
bkorshunov <mrdrakula164@gmail.com>
2025-11-27 23:59:37 +0500

    https://ims.cr5.space/app/p/7p2YwVX2/village-keeper/tasks/65
    Обновлён визуал AdrenalineGain

--- Assets/! VillageProject/UI/Gameplay/HUD/Adrenaline/Scripts/AdrenalineGainDisplay.cs
+++ Assets/! VillageProject/UI/Gameplay/HUD/Adrenaline/Scripts/AdrenalineGainDisplay.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class AdrenalineGainDisplay : SignalListener<StaminaSignalInvoker.AdrenalineChangedSignal>
 {
     [SerializeField] private AdrenalineGainEffect _vfx;
+    [SerializeField] private AnimationCurve _amountToSize;
     private float _cachedValue;
 
     protected override void OnSignal(StaminaSignalInvoker.AdrenalineChangedSignal signal)
@@ -10,7 +11,8 @@ public class AdrenalineGainDisplay : SignalListener<StaminaSignalInvoker.Adrenal
         if (signal.Value > _cachedValue)
         {
             var effect = Instantiate(_vfx, transform);
-            effect.SetSize(signal.Value - _cachedValue);
+            var size = _amountToSize.Evaluate(signal.Value - _cachedValue);
+            effect.SetSize(size);
         }
         _cachedValue = signal.Value;
     }
